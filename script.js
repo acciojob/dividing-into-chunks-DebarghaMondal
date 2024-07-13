17c0b5ffc9cfcd8ff3c36e26a7860fec9391af66
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-13 16:58:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+  const result = arr.reduce((resultArray, item, index) => { 
+  const chunkIndex = Math.floor(index/n)
+
+  if(!resultArray[chunkIndex]) {
+    resultArray[chunkIndex] = [] // start a new chunk
+  }
+
+  resultArray[chunkIndex].push(item)
+
+  return resultArray
 };
 
 const n = prompt("Enter n: ");
